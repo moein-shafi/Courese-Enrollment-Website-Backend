b3b20fa624f9a7f908a3e0a63fe206e8ff2ba0b6
moein.shafi <mosafer.moein@gmail.com>
2021-04-18 16:11:41 +0430

    Add 'logout' controller.

--- src/main/java/ie/diyar_moein_ca5/controllers/LoginController.java
+++ src/main/java/ie/diyar_moein_ca5/controllers/LoginController.java
@@ -2,6 +2,8 @@ package ie.diyar_moein_ca5.controllers;
 
 import ie.diyar_moein_ca5.Classes.Database;
 import ie.diyar_moein_ca5.Exceptions.StudentNotFoundException;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,19 +13,29 @@ import java.util.HashMap;
 @RestController
 public class LoginController {
 
-    @PostMapping("/login")
+    @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
     public HashMap<String, String> hello(@RequestParam(value = "studentId") String studentId) {
         Database database = Database.getDatabase();
         HashMap<String, String> response = new HashMap<>();
         try {
             database.setCurrentStudent(studentId);
             response.put("code", "200");
-            response.put("message", "login successful");
+            response.put("message", "login successfully");
         } catch (StudentNotFoundException e) {
             database.setErrorMessage("Student Not Found!");
             response.put("code", "404");
             response.put("message", database.getErrorMessage());
         }
+        return response;
+    }
+
+    @DeleteMapping("/login", produces = MediaType.APPLICATION_JSON_VALUE)
+    public HashMap<String, String> hello() {
+        Database database = Database.getDatabase();
+        HashMap<String, String> response = new HashMap<>();
+        database.logout();
+        response.put("code", "200");
+        response.put("message", "logged out successfully");
         return response;
     }
 }
